2db5aae6ad275578ee0ae302f57ab2cdc0278492
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2023-12-09 16:59:14 +0530

    added

---
+++ myportfolio/src/Components/About/About.js
@@ -0,0 +1,126 @@
+import { Box, Button, Slide, Typography, Zoom, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import "./About.css";
+import RajshreeJaiswal from "../../media/RajshreeJaiswal.png";
+import LinearDeterminate from "../MiniComponent/ProgressComp";
+import RAJSHREE_JAISWAL from "../../media/RAJSHREE_JAISWAL.pdf";
+
+const About = () => {
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [scrolled, setScrolled] = useState(0);
+
+  window.addEventListener("scroll", () => {
+    setScrolled(document.documentElement.scrollTop);
+  });
+
+  if (scrolled >= 56) {
+    setTimeout(() => {
+      setChecked1(true);
+    }, 600);
+    setTimeout(() => {
+      setChecked2(true);
+    }, 800);
+  }
+
+  const openResume = () => {
+    window.open(
+      "https://drive.google.com/file/d/1SdQbGgeuZ0Iyvb0UnvxyTiqVRTVC3cg6/view?usp=sharing",//resume link
+      "_blank"
+    );
+  };
+
+  return (
+    <Grid
+      container
+      id="about"
+      className="about section"
+      sx={{ width: "80%", margin: "10% auto", padding: "0% 10px" }}
+      spacing={2}
+    >
+      <Grid item xs={12} sm={6} md={6} sx={{ margin: "auto" }}>
+        <Box
+          sx={{ borderTop: "2px solid rgb(35, 53, 84)", position: "relative" }}
+        >
+          <Typography
+            className="aboutHeading"
+            variant="h4"
+            sx={{
+              backgroundColor: "rgb(11, 24, 47)",
+              position: "absolute",
+              top: "-25px",
+              padding: "0% 5% 0% 0%",
+              fontFamily: "Poppins",
+              color: "white",
+            }}
+          >
+            About Me
+          </Typography>
+          <Typography
+            id="user-detail-intro"
+            className="aboutText"
+            sx={{
+              marginTop: "8%",
+              color: "rgb(136,146,176)",
+              fontFamily: "Poppins",
+            }}
+          >
+           Hi, I'm Md Sajid Ansari{" "}
+            <span style={{ color: "white" }}>Md Sajid Ansari</span> and I thrive on bringing digital creations to life. The vast possibilities of the internet have always fascinated me, 
+            and my passion is centered around crafting websites that not only impress visually but also bring joy to users.
+            <br />
+            <br />
+            Since graduating I ventured into{" "}
+            <span style={{ color: "white" }}>Web Development</span> working with tech stacks like html, css,JavaScript,react, Node.js, Express.js, 
+            and MongoDB on diverse projects. Currently, my focus is on developing products that are accessible and inclusive. 
+            As an enthusiastic Full Stack Developer, I am a perpetual learner,
+            specializing in front-end Web Development with a keen eye for detail and a high adaptability to 
+            emerging technologies. Let's collaborate to create exceptional online experiences!
+          </Typography>
+          <Box
+            id="resumeBtn"
+            onClick={openResume}
+            marginTop="1.5rem"
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Button
+              variant="outlined"
+               href={RAJSHREE_JAISWAL}//resume
+              download="Rajshree-Jaiswal-Resume"
+              id="resume-button-2"
+              sx={{
+                border: "1px solid #008F11",
+                color: "white",
+                fontSize: "0.8rem",
+                fontFamily: "Poppins",
+                margin: "auto",
+              }}
+            >
+              Resume
+            </Button>
+            <LinearDeterminate color={"transparent"} />
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ margin: "auto" }}>
+        <Box
+          className="imageContainer"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            className="home-img"
+            style={{ display: "block", margin: "auto", borderRadius: "5px" }}
+            width={"60%"}
+            // src={} //image of portfolio
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default About;
---
+++ myportfolio/src/Components/Contact/Contact.js
@@ -0,0 +1,143 @@
+import { Box, Button, Typography, Zoom } from "@mui/material";
+import React, { useState } from "react";
+import { details } from "../../details";
+import "./Contact.css";
+
+const Contact = () => {
+  const [checked1, setChecked1] = useState(false);
+
+  const [scrolled, setScrolled] = useState(0);
+
+  window.addEventListener("scroll", () => {
+    setScrolled(document.documentElement.scrollTop);
+  });
+
+  if (scrolled >= 2700) {
+    setTimeout(() => {
+      setChecked1(true);
+    }, 500);
+  }
+
+  const mailTo = () => {
+    window.open("mailto:rajshreejaiswal0907@gmail.com", "_blank");
+  };
+  return (
+    <Zoom className="contactDiv" in={checked1}>
+      <Box
+        id="contact"
+        sx={{ width: "80%", margin: "10% auto", padding: "0% 10px" }}
+      >
+        <Typography
+          className="contactheading"
+          sx={{
+            textAlign: "center",
+            fontWeight: "600",
+            color: "rgb(204,214,246)",
+          }}
+          variant="h2"
+        >
+          Get In Touch
+        </Typography>
+        <Typography
+          className="contactSubHeading"
+          sx={{
+            textAlign: "center",
+            width: "70%",
+            margin: "auto",
+            color: "rgb(124,134,164)",
+          }}
+          variant="h5"
+        >
+          
+
+          I'm actively exploring new opportunities, and my inbox is open for any inquiries or messages. 
+          Feel free to reach out with questions or just to say hello—I'll do my best to respond promptly!
+        </Typography>
+
+        <Button
+          className="contactButton"
+          onClick={mailTo}
+          variant="outlined"
+          sx={{
+            border: "1px solid black",
+            boxShadow: "0 0 10px green",
+            color: "white",
+            fontSize: "0.9rem",
+            margin: "auto",
+            padding: "1% 3%",
+            display: "block",
+            marginTop: "2%",
+          }}
+        >
+          Say Hello
+        </Button>
+        <Box
+          sx={{
+            marginTop: "2%",
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            gap: "15px",
+            flexDirection: "column",
+          }}
+        >
+          <a
+            id="contact-github"
+            className="socialLogo"
+            target={"_blank"}
+            href={details.githubLink}
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "1.7rem",
+            }}
+          >
+            <i className="fa-brands fa-github"></i> Md Sajid Ansari
+          </a>
+          <a
+            id="contact-linkedin"
+            className="socialLogo"
+            target={"_blank"}
+            href={details.linkedInLink}
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "1.7rem",
+            }}
+          >
+            <i className="fa-brands fa-linkedin"></i> Md Sajid Ansari
+          </a>
+          <a
+            id="contact-phone"
+            className="socialLogo"
+            target={"_blank"}
+            href={"#"}
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "1.7rem",
+            }}
+          >
+            <i className="fa-solid fa-phone"></i> +91-7061541556
+          </a>
+          <a
+            id="contact-email"
+            className="socialLogo"
+            target={"_blank"}
+            href={details.mailLink}
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "1.5rem",
+            }}
+          >
+            <i className="fa-regular fa-envelope"></i>mdsajidalam8340@gmail.com
+          </a>
+        </Box>
+      </Box>
+    </Zoom>
+  );
+};
+
+export default Contact;
